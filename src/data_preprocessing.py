28b92f9bbb6eb9f16a0ca518f869a0da1e62b12c
Mateusz Witka-Jeżewski <m.w.jezewski@gmail.com>
2024-09-05 22:30:05 +0200

    other files

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,19 +1,19 @@
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import os
 import json
-from PIL import Image
-import torch
-from torch.utils.data import Dataset, DataLoader
+import os
+
 import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
 
 class CustomImageDataset(Dataset):
     def __init__(self, folder_path, label_mapping_path, transform=None):
         self.folder_path = folder_path
         self.transform = transform
-        self.image_filenames = [f for f in os.listdir(folder_path) if f.endswith(('.jpg', '.png', '.jpeg'))]  # Adjust file extensions as needed
-        with open(label_mapping_path, 'r') as f:
-            self.label_mapping = json.load(f) 
+        self.image_filenames = [f for f in os.listdir(folder_path) if f.endswith((".jpg", ".png", ".jpeg"))]
+        with open(label_mapping_path, "r") as f:
+            self.label_mapping = json.load(f)
 
     def __len__(self):
         return len(self.image_filenames)
@@ -21,32 +21,44 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         img_name = self.image_filenames[idx]
         img_path = os.path.join(self.folder_path, img_name)
-        image = Image.open(img_path).convert('RGB')
-
+        image = Image.open(img_path).convert("RGB")
         if self.transform:
             image = self.transform(image)
 
-        label_name = img_name.rsplit('.', 1)[0] + '.json'  # Replace image extension with .json
+        label_name = (img_name.rsplit(".", 1)[0] + ".json")
         label_path = os.path.join(self.folder_path, label_name)
 
-        with open(label_path, 'r') as f:
+        with open(label_path, "r") as f:
             label_data = json.load(f)
-            label_str = label_data['country_name']
+            label_str = label_data["country_name"]
             label = self.label_mapping[label_str]
 
         return image, label
 
 
-def get_dataloader(test_data: bool=True) -> DataLoader:
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-    ])
+class DataLoaders:
+    def __init__(self, trial_data: bool = True):
+        self._transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+        ])
+        self._label_mapping_path = "../data/country_to_index.json"
+        self._base_path = "../data/trial_data" if trial_data else "data/full_data"
 
-    if test_data:
-        path = "../data/test_data"
-    else:
-        path = "data/full_data"
-    dataset = CustomImageDataset(folder_path=path, label_mapping_path="../data/country_to_index.json", transform=transform)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
-    return dataloader
+        self._train_dataloader = self._create_dataloader("train")
+        self._test_dataloader = self._create_dataloader("test")
+
+    def _create_dataloader(self, data_type: str) -> DataLoader:
+        path = f"{self._base_path}/{data_type}"
+        dataset = CustomImageDataset(
+            folder_path=path,
+            label_mapping_path=self._label_mapping_path,
+            transform=self._transform,
+        )
+        return DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
+
+    def get_train_dataloader(self) -> DataLoader:
+        return self._train_dataloader
+
+    def get_test_dataloader(self) -> DataLoader:
+        return self._test_dataloader
