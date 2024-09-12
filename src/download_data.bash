dbacb72191ee529e264bc991f5f303baf7e7fa3e
Mateusz Witka-Jeżewski <m.w.jezewski@gmail.com>
2024-09-12 19:31:19 +0200

    add req.txt

--- src/download_data.bash
+++ src/download_data.bash
@@ -2,7 +2,6 @@
 
 source .env
 
-# List of datasets to download
 datasets=(
     "killusions/street-location-images"
     "ubitquitin/geolocation-geoguessr-images-50k"
@@ -10,18 +9,13 @@ datasets=(
 
 download_dir="data/full_data"
 
-# Set up your Kaggle API token (replace with your actual token)
 kaggle config set -n API_TOKEN -v "$KAGGLE_API_TOKEN"
 
-# Loop through each dataset and download it
 for dataset in "${datasets[@]}"; do
-    # Extract the dataset name without the username (after the '/')
     dataset_name=$(basename "$dataset")
 
-    # Download the dataset
     kaggle datasets download -d "$dataset" -p "$download_dir"
 
-    # Unzip the dataset if it is a zip file
     zip_file="$download_dir/${dataset_name}.zip"
     echo $zip_file
     if [[ -f "$zip_file" ]]; then
